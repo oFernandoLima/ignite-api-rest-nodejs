63c417b0b74c9c9aaec2fd7306106cb7a42db950
oFernandoLima <eufernandoprogramador@gmail.com>
2023-06-20 13:31:34 -0300

    database creation with knex

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
-import fastify from "fastify";
+import fastify from "fastify"
+import crypto from "node:crypto"
+import { knex } from "./database"
+import { env } from "./env"
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello World!";
-});
+app.get("/hello", async () => {
+  const transaction = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*")
+
+  return transaction
+})
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log("HTTP Server Running!");
-  });
+    console.log("HTTP Server Running!")
+  })
